a8357627caec59e7392124e705bd2324b65efee7
BiplabM <71537845+biplabmazumder2020@users.noreply.github.com>
2020-10-10 14:30:10 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('biplabmazumder2020/seedjob')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
